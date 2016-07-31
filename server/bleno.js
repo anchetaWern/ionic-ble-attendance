2d1e656029ba94e53fef1d11e5097837fd390509
Wern Ancheta <ancheta.wern@gmail.com>
2016-07-31 21:47:02 +0800

    initialize project

---
+++ server/bleno.js
@@ -0,0 +1,41 @@
+var bleno = require('bleno');
+
+var attendees = [];
+var settings = {
+  service_id: '24ui',
+  characteristic_id: '70xz'
+};
+
+bleno.on('stateChange', function(state){
+  if(state === 'poweredOn'){
+    bleno.startAdvertising('AttendanceApp', ['24ui']);
+  }else{
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(error){
+    if(error){
+      // error on advertise start
+    }else{
+      bleno.setServices([
+        new bleno.PrimaryService({
+          uuid : settings.service_id,
+          characteristics : [
+            new bleno.Characteristic({
+              value : null,
+              uuid : settings.characteristic_id,
+              properties : ['read', 'write'],
+              onWriteRequest : function(data, offset, withoutResponse, callback){
+                var attendee = JSON.parse(data.toString());
+                attendee.time_entered = Date.now();
+                attendees.push(attendee);
+                console.log(attendees);
+                callback(this.RESULT_SUCCESS);
+              }
+            })
+          ]
+        })
+      ]);
+    }
+});
---
+++ www/js/controllers/DeviceController.js
@@ -0,0 +1,47 @@
+(function(){
+  angular.module('starter')
+  .controller('DeviceController', ['$scope', '$state', '$stateParams', 'DeviceFactory', DeviceController]);
+
+  function DeviceController($scope, $state, $stateParams, DeviceFactory){
+
+    var me = this;
+
+    var service_id = '24ui';
+    var characteristic_id = '70xz';
+
+    me.attendee = {
+      firstname: '',
+      lastname: ''
+    }
+
+    $scope.init = function(){
+      $scope.device = DeviceFactory.getDevice($stateParams.id);
+    }
+
+    $scope.attend = function(){
+      ble.write(
+        $stateParams.id,
+        service_id,
+        characteristic_id,
+        btoa(JSON.stringify(me.attendee)),
+        function(response){
+          if(response == 'OK'){
+            alert("Your attendance is recorded!");
+            ble.disconnect($stateParams.id);
+            $state.go('home');
+          }
+        },
+        function(err){
+          alert("Error occured while trying to record your attendance. Please try again.");
+        }
+      );
+    }
+
+    $scope.backToHome = function(){
+      $state.go('home');
+      ble.disconnect($stateParams.id);
+    }
+
+  }
+
+})();
